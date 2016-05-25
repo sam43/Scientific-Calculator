1777300521935878df20585e0eea2c70b22b0239
sam43 <scode43@gmail.com>
2016-05-26 00:08:17 +0600

    Add files via upload

---
+++ src/cal/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cal;
+
+/**
+ *
+ * @author scode
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new CAL().setVisible(true);
+
+    }
+
+}
+
